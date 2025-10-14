d131f86fa2c86e8139f02fd0cad51ba17c3b24ee
Jaume Salas <jsalas2@nigul.cide.es>
2025-10-14 16:29:27 +0000

    d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log("todo fino");
 
+const app = require('./app');
+const {PORT} = process.env;
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working as expected</h1>");
+});
+
+module.exports = app;

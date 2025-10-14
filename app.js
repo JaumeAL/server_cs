2a0e98e6252a1acbc2065ed3280c4f05c8964734
Jaume Salas <jsalas2@nigul.cide.es>
2025-10-14 16:37:07 +0000

    jaume

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World jaume</h1>');
+});
+
+
+module.exports = app;
